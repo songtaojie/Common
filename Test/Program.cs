3ba2e3408de38a89b1723a323adedfc35206cdb6
songtaojie <songtaojie@aio5.com>
2018-10-10 17:56:06 +0800

    添加帮助类

---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using Common.Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TypeHelper.GetInt16(null));
+            Console.ReadLine();
+        }
+    }
+}
