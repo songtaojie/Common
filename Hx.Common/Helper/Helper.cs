5da7e8ff22f433a656d16bc92f5242c67aa45fa9
songtaojie <stjworkemail@163.com>
2019-07-30 17:55:16 +0800

    修改帮助类

--- Hx.Common/Helper/Helper.cs
+++ Hx.Common/Helper/Helper.cs
@@ -451,26 +451,5 @@ namespace Hx.Common.Helper
             return (fahrenheit - 32) * 5 / 9;
         }
         #endregion
-
-        /// <summary>
-        /// 根据字节大小获取文件的大小描述
-        /// </summary>
-        /// <param name="Size"></param>
-        /// <returns></returns>
-        public static string GetFileSizeDes(long Size)
-        {
-            string strSize = "";
-            long FactSize = 0;
-            FactSize = Size;
-            if (FactSize < 1024.00)
-                strSize = FactSize.ToString("F2") + " Byte";
-            else if (FactSize >= 1024.00 && FactSize < 1048576)
-                strSize = (FactSize / 1024.00).ToString("F2") + " K";
-            else if (FactSize >= 1048576 && FactSize < 1073741824)
-                strSize = (FactSize / 1024.00 / 1024.00).ToString("F2") + " M";
-            else if (FactSize >= 1073741824)
-                strSize = (FactSize / 1024.00 / 1024.00 / 1024.00).ToString("F2") + " G";
-            return strSize;
-        }
     }
 }
