a1096968388dd54ddba705e7d18e074880cd1977
songtaojie <stjworkemail@163.com>
2021-01-05 17:40:29 +0800

    添加DBSession

--- Hx.Sdk.EntityFrameworkCore/Context/IDbSession.cs
+++ Hx.Sdk.EntityFrameworkCore/Context/IDbSession.cs
@@ -5,7 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Hx.Sdk.EntityFrameworkCore.Context
+namespace Hx.Sdk.EntityFrameworkCore.Db
 {
     /// <summary>
     /// 数据库上下文操作类
---
+++ Hx.Sdk.EntityFrameworkCore/DbSessionExtension.cs
@@ -0,0 +1,23 @@
+﻿using Hx.Sdk.EntityFrameworkCore.Db;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hx.Sdk.EntityFrameworkCore
+{
+    public static class DbSessionExtension
+    {
+        /// <summary>
+        /// 添加数据库上下文
+        /// </summary>
+        /// <param name="services">服务集合</param>
+        public static void AddDbSession<T>(this IServiceCollection services)where T:DbContext
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            services.AddDbContext<T>();
+            services.AddScoped<IDbSession, DbSession>();
+        }
+    }
+}
