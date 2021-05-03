1f9100a45f77e913cf1ba870024a93c7608f5411
songtaojie <stjworkemail@163.com>
2021-05-03 18:01:55 +0800

    fix entity

--- Hx.Sdk.Entity/DatabaseAccessor/Base/EntityBase.cs
+++ Hx.Sdk.Entity/DatabaseAccessor/Base/EntityBase.cs
@@ -4,6 +4,13 @@ namespace Hx.Sdk.Entity
 {
 
     /// <summary>
+    /// 数据库实体依赖基类(默认为字符串主键)
+    /// </summary>
+    [SkipScan]
+    public abstract class EntityBase : EntityBase<string>
+    { }
+
+    /// <summary>
     /// 数据库实体依赖基类
     /// </summary>
     /// <typeparam name="TKeyType">主键类型</typeparam>
--- Hx.Sdk.Entity/DatabaseAccessor/Base/StatusEntityBase.cs
+++ Hx.Sdk.Entity/DatabaseAccessor/Base/StatusEntityBase.cs
@@ -4,6 +4,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace Hx.Sdk.Entity
 {
     /// <summary>
+    ///  该基类封装了删除，禁用等状态字段（默认为字符串主键）
+    /// </summary>
+    [SkipScan]
+    public abstract class StatusEntityBase : StatusEntityBase<string>
+    { }
+
+    /// <summary>
     ///  该基类封装了删除，禁用等状态字段
     /// </summary>
     /// <typeparam name="TKeyType">主键的类型</typeparam>
