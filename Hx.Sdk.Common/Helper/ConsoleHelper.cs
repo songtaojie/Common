7ee689ea67e9f1eb8e68d89175b7f80ad4271ff2
songtaojie <stjworkemail@163.com>
2021-05-08 16:52:08 +0800

    update hx.sdk.common

--- Hx.Sdk.Common/Helper/ConsoleHelper.cs
+++ Hx.Sdk.Common/Helper/ConsoleHelper.cs
@@ -9,50 +9,55 @@ namespace Hx.Sdk.Common.Helper
     /// </summary>
     public static class ConsoleHelper
     {
-        static void WriteColorLine(string str, ConsoleColor color)
+        static void WriteColorLine(string str, ConsoleColor color,bool newLine = false)
         {
             ConsoleColor currentForeColor = Console.ForegroundColor;
             Console.ForegroundColor = color;
             Console.WriteLine(str);
             Console.ForegroundColor = currentForeColor;
+            if (newLine) Console.WriteLine();
         }
 
         /// <summary>
         /// 打印错误信息
         /// </summary>
         /// <param name="str">待打印的字符串</param>
+        /// <param name="newLine">是否在后面添加一行空行</param>
         /// <param name="color">想要打印的颜色</param>
-        public static void WriteErrorLine(this string str, ConsoleColor color = ConsoleColor.Red)
+        public static void WriteErrorLine(this string str, bool newLine = false, ConsoleColor color = ConsoleColor.Red)
         {
-            WriteColorLine(str, color);
+            WriteColorLine(str, color, newLine);
         }
 
         /// <summary>
         /// 打印警告信息
         /// </summary>
         /// <param name="str">待打印的字符串</param>
+        /// <param name="newLine">是否在后面添加一行空行</param>
         /// <param name="color">想要打印的颜色</param>
-        public static void WriteWarningLine(this string str, ConsoleColor color = ConsoleColor.Yellow)
+        public static void WriteWarningLine(this string str, bool newLine = false, ConsoleColor color = ConsoleColor.Yellow)
         {
-            WriteColorLine(str, color);
+            WriteColorLine(str, color, newLine);
         }
         /// <summary>
         /// 打印正常信息
         /// </summary>
         /// <param name="str">待打印的字符串</param>
+        /// <param name="newLine">是否在后面添加一行空行</param>
         /// <param name="color">想要打印的颜色</param>
-        public static void WriteInfoLine(this string str, ConsoleColor color = ConsoleColor.White)
+        public static void WriteInfoLine(this string str, bool newLine = false, ConsoleColor color = ConsoleColor.White)
         {
-            WriteColorLine(str, color);
+            WriteColorLine(str, color, newLine);
         }
         /// <summary>
         /// 打印成功的信息
         /// </summary>
         /// <param name="str">待打印的字符串</param>
+        /// <param name="newLine">是否在后面添加一行空行</param>
         /// <param name="color">想要打印的颜色</param>
-        public static void WriteSuccessLine(this string str, ConsoleColor color = ConsoleColor.Green)
+        public static void WriteSuccessLine(this string str, bool newLine = false,ConsoleColor color = ConsoleColor.Green)
         {
-            WriteColorLine(str, color);
+            WriteColorLine(str, color, newLine);
         }
 
     }
